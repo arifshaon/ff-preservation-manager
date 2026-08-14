ac6de29f4294b7ad8e1ba8584cff3a58118f1fae
Arif Shaon <ashaon@qf.org.qa>
2026-08-14 15:59:10 +0300

    Register NARA preservation CSV adapter

--- qnl_format_registry_builder/registry_builder/adapters/__init__.py
+++ qnl_format_registry_builder/registry_builder/adapters/__init__.py
@@ -6,12 +6,14 @@ from registry_builder.adapters.pronom_droid_xml import PronomDroidXmlAdapter
 from registry_builder.adapters.loc_fdd_xml import LocFddXmlAdapter
 from registry_builder.adapters.institution_policy_xlsx import InstitutionPolicyXlsxAdapter
 from registry_builder.adapters.qnl_policy_xlsx import QnlPolicyXlsxAdapter
+from registry_builder.adapters.nara_preservation_csv import NaraPreservationCsvAdapter
 
 ADAPTERS: dict[str, type[SourceAdapter]] = {
     StandardJsonAdapter.type_name: StandardJsonAdapter,
     PronomDroidXmlAdapter.type_name: PronomDroidXmlAdapter,
     LocFddXmlAdapter.type_name: LocFddXmlAdapter,
     InstitutionPolicyXlsxAdapter.type_name: InstitutionPolicyXlsxAdapter,
+    NaraPreservationCsvAdapter.type_name: NaraPreservationCsvAdapter,
     # Deprecated compatibility alias. Prefer institution_policy_xlsx.
     QnlPolicyXlsxAdapter.type_name: QnlPolicyXlsxAdapter,
 }
